cc428be2813bed9638dabd6ed97f45cc82826de0
Arash Soheili <tonyarash@gmail.com>
2017-07-30 11:17:31 -0400

    Convert Slide component to stateless function

--- src/slide.js
+++ src/slide.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class Slider extends Component {
-  render() {
-    const width = this.props.width || 300;
-    const height = this.props.height || 600;
-    const backgroundImage = require(`./img/${this.props.image}`);
+const Slider = ({image, width, height}) => {
+    const backgroundImage = require(`./img/${image}`);
     const styles = {
-      imageBackground: {
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundPosition: 'center top',
-        backgroundRepeat: 'no-repeat',
-        float: 'left',
-        width: `${width}px`,
-        height: `${height}px`
-      }
+      backgroundImage: `url(${backgroundImage})`,
+      backgroundPosition: 'center top',
+      backgroundRepeat: 'no-repeat',
+      float: 'left',
+      width: `${width}px`,
+      height: `${height}px`
     }
     return (
-      <div className="slide" style={styles.imageBackground}></div>
+      <div className="slide" style={styles}></div>
     )
-  }
 }
+
+Slider.defaultProps = {
+  width: 300,
+  height: 600
+}
+
+Slider.propTypes = {
+  image: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired
+}
+
+export default Slider;
